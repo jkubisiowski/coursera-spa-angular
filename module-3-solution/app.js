331c90afb3037bc9f541497c93e3ec497c0c4efd
jakub <jkubisiowski>
2016-10-06 20:58:14 +0200

    Implement module-3-solution

--- module-3-solution/app.js
+++ module-3-solution/app.js
@@ -4,20 +4,26 @@
 angular.module('Menu', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective)
 NarrowItDownController.$inject = ['$scope', 'MenuSearchService'];
 MenuSearchService.$inject = ['$http'];
 
 function NarrowItDownController($scope, MenuSearchService) {
-  var menu = this;
-  menu.searchTerm = "";
+  var narrow = this;
+  narrow.searchTerm = "";
+  narrow.found = []
+  narrow.empty = false;
 
-  menu.narrowIt = function()
+  narrow.narrowIt = function()
   {
-    MenuSearchService.getMatchedMenuItem(menu.searchTerm).then(function(items)
-    {
-      console.log(items);
-    })
-
+    MenuSearchService.getMatchedMenuItem(narrow.searchTerm).then(function(items) {
+      narrow.found = items;
+      if(items.length==0)
+        narrow.empty = true;
+    });
+  }
+  narrow.remove = function(index) {
+    narrow.found.splice(index,1);
   }
 }
 
@@ -31,12 +37,22 @@ function MenuSearchService($http){
     // process result and only keep items that match
     var foundItems = result.data.menu_items;
     // return processed items
-    return foundItems.filter(function(item)
-    {
+    return foundItems.filter(function(item) {
         return item.description.includes(searchTerm)
     })
     });
   }
+}
+
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: 'menuList.html',
+    scope: {
+      items: '<',
+      onRemove: '&'
+    }
+  };
+  return ddo;
 }
 
 }
